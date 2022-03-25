61dd62a579d1f23fd89ade9ed5f5937b35effec4
saladavid0110 <saladavid0110@gmail.com>
2022-03-24 21:01:06 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import GifExpertApp from './Components/GifExpertApp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='header'>
+          <GifExpertApp />
+        </div>
+      </div>
     </div>
   );
 }
